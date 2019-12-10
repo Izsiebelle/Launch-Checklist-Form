477a43ffab33a79ce5a1db43d848a356b8a7439f
izsie <izsie.robinson@gmail.com>
2019-12-09 19:25:18 -0600

    validated inputs

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+   let pilotNameInput = document.querySelector("input[name=pilotName]");
+   let coPilotNameInput = document.querySelector("input[name=copilotName]");
+   let fuleLevelInput = document.querySelector("input[name=fuelLevel]");
+   let cargoMassInput = document.querySelector("input[name=cargoMass]");
+
+   if (pilotNameInput.value === "" || coPilotNameInput.value === "" || fuleLevelInput.value === "" || cargoMassInput.value === "") {
+      alert("All fields are required!");
+   }
+
+   if(!isNaN(pilotNameInput.value)){
+      alert("Pilot name needs to be letters")
+   }
+
+   if(!isNaN(coPilotNameInput.value)){
+      alert("Co Pilot name needs to be letters")
+   }
+
+   if(isNaN(fuleLevelInput)){
+      alert("Fuel Level needs to be a number")
+   }
+
+   if(isNaN(cargoMassInput.value)){
+      alert("Cargo Mass needs to be a number")
+   }
+
+
+   
+
+   event.preventDefault();
+});
+
+})
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
@@ -11,3 +48,4 @@
 </ol>
 <img src="${}">
 */
+
